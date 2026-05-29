0aba91b2e4a6addff341cb16f52f68a88cb95916
nunoedgar <edgarpedra99@gmail.com>
2026-05-29 13:22:49 +0100

    Initial commit

---
+++ src/main/java/com/nunostudios/securityessentials/SecurityEssentialsApplication.java
@@ -0,0 +1,13 @@
+package com.nunostudios.securityessentials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityEssentialsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityEssentialsApplication.class, args);
+    }
+
+}
